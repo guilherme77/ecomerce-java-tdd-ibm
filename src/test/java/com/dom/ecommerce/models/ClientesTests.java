d320249714a9263b803c2f950cf735c4947a1932
guilherme77 <guilhermesilva986@gmail.com>
2022-09-07 13:59:39 -0300

    Init

---
+++ src/test/java/com/dom/ecommerce/models/ClientesTests.java
@@ -0,0 +1,39 @@
+package com.dom.ecommerce.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesTests {
+
+	@Test
+	void validarPropriedades() {
+
+		var cliente = new Cliente();
+
+		cliente.setNome("Guilherme");
+		cliente.setEmail("guilherme@teste.com");
+		cliente.setSenha("123456");
+		cliente.setEndereco("Rua Teste");
+		cliente.setNumero("T7");
+		cliente.setCep("0000-000");
+		cliente.setCidade("Recife");
+		cliente.setBairro("Boa Vista");
+		cliente.setEstado("PE");
+		cliente.setComplemento("Casa 7");
+
+		assertEquals("Guilherme", cliente.getNome());
+		assertEquals("guilherme@teste.com", cliente.getEmail());
+		assertEquals("123456", cliente.getSenha());
+		assertEquals("Rua Teste", cliente.getNumero());
+		assertEquals("T7", cliente.getCep());
+		assertEquals("Recife", cliente.getCidade());
+		assertEquals("Boa Vista", cliente.getBairro());
+		assertEquals("PE", cliente.getEstado());
+		assertEquals("Casa 7", cliente.getComplemento());
+
+	}
+
+}
